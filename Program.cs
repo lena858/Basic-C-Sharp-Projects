bb8977595306db3315b3edd663abdb1aa6adfec3
lena858 <solaf-2011-s@hotmail.com>
2025-08-30 21:43:00 -0600

    Add files via upload
    Added Package Express project

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.Write("Please enter the package weight: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        Console.Write("Please enter the package width: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please enter the package height: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please enter the package length: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return;
+        }
+
+        double dimensionsProduct = width * height * length;
+        double quote = (dimensionsProduct * weight) / 100;
+
+        Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+        Console.WriteLine("Thank you!");
+    }
+}
+
